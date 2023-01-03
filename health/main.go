97a093aa92bbf803f05cc37ffabe82cd8e88811e
Mt <matjaz.zupanic1@gmail.com>
2023-01-03 11:53:26 +0100

    Liveness

--- health/main.go
+++ health/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
@@ -9,6 +10,89 @@ import (
 	"time"
 )
 
+type healthCheck struct {
+	Id     string `json:"id"`
+	Health []struct {
+		Name string `json:"name"`
+		// Status of the health check
+		Status string `json:"status"`
+		// Error message of the health check
+		Error []string `json:"error"`
+		// Timestamp of the health check
+		Timestamp string `json:"timestamp"`
+	} `json:"types"`
+	// Name of the health check
+
+}
+
+//	func test(url string, txt string) string {
+//		heal := healthCheck{
+//			Name:      "Container",
+//			Status:    "ERROR",
+//			Error:     nil,
+//
+//		}
+//		spaceClient := http.Client{Timeout: time.Second * 20} // Timeout after 2 seconds
+//		req, err := http.NewRequest(http.MethodGet, url, nil)
+//		if err != nil {
+//			heal.Error = append(heal.Error, txt + err.Error())
+//			heal.Timestamp = time.Now().Format(time.RFC3339)
+//			// convert heal to string
+//			healString, _ := json.Marshal(heal)
+//			return string(healString)
+//		}
+//		req.Header.Set("User-Agent", "test")
+//		res, getErr := spaceClient.Do(req)
+//		if getErr != nil {
+//			heal.Error = append(heal.Error, txt + getErr.Error())
+//			heal.Timestamp = time.Now().Format(time.RFC3339)
+//			// convert heal to string
+//			healString, _ := json.Marshal(heal)
+//			return string(healString)
+//		}
+//		if res.Body != nil {
+//			defer res.Body.Close()
+//		}
+//
+//		body, readErr := ioutil.ReadAll(res.Body)
+//		if readErr != nil {
+//			heal.Error = append(heal.Error, txt + readErr.Error())
+//			heal.Timestamp = time.Now().Format(time.RFC3339)
+//			// convert heal to string
+//			healString, _ := json.Marshal(heal)
+//			return string(healString)
+//		}
+//		return string(body)
+//	}
+func checkFNC(url string, txt string) int {
+	spaceClient := http.Client{Timeout: time.Second * 20} // Timeout after 2 seconds
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return 500
+	}
+	req.Header.Set("User-Agent", "test")
+	res, getErr := spaceClient.Do(req)
+	if getErr != nil {
+
+		return 500
+	}
+	if res.Body != nil {
+		defer res.Body.Close()
+	}
+
+	body, readErr := ioutil.ReadAll(res.Body)
+	if readErr != nil {
+		return 500
+	}
+	//body to arrayHealthCheck
+	var array healthCheck
+	json.Unmarshal(body, &array)
+	if array.Health[0].Status != "OK" {
+		return 500
+	} else {
+		return 200
+	}
+}
 func checkAut() string {
 	url := "http://104.45.183.75/authentication"
 	//url := "http://authentication:8003/"                  //weather
@@ -108,6 +192,7 @@ func checkWeather() string {
 	}
 	return string(body)
 }
+
 func main() {
 
 	// Expose API
@@ -129,16 +214,23 @@ func main() {
 		return c.SendString("Dela")
 
 	})
-	started := time.Now()
+	//started := time.Now()
 	http.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
-		duration := time.Now().Sub(started)
-		if duration.Seconds() > 10 {
-			w.WriteHeader(500)
-			w.Write([]byte(fmt.Sprintf("error: %v", duration.Seconds())))
+		res := checkFNC("http://104.45.183.75/authentication", "Authentication")
+		w.WriteHeader(res)
+		if res == 200 {
+			w.Write([]byte("ok ; time: " + time.Now().String()))
 		} else {
-			w.WriteHeader(200)
-			w.Write([]byte("ok"))
+			w.Write([]byte("error; time: " + time.Now().String()))
 		}
+		//duration := time.Now().Sub(started)
+		//if duration.Seconds() > 10 {
+		//	w.WriteHeader(500)
+		//	w.Write([]byte(fmt.Sprintf("error: %v", duration.Seconds())))
+		//} else {
+		//	w.WriteHeader(200)
+		//	w.Write([]byte("ok"))
+		//}
 		//started = time.Now()
 	})
 	app.Listen(":8080")
